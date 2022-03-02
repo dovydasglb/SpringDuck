0ea84ee76927ad61b2d77eff26ed999f0643de78
dovydasglb <dovydasglb@gmail.com>
2022-03-02 10:22:37 +0000

    Initial commit

---
+++ src/main/java/com/qa/springduck/SpringDuckApplication.java
@@ -0,0 +1,13 @@
+package com.qa.springduck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDuckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDuckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qa/springduck/SpringDuckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.springduck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDuckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
